96172fbfce7ebcac32238d58cc32efa3bf760cd1
79SUBTEAM <91619620+79SUBTEAM@users.noreply.github.com>
2024-10-11 12:09:16 +0700

    Add files via upload

---
+++ assets/js/nguyenduyhoang.js
@@ -0,0 +1,29 @@
+console.log(
+    "%c Coder : Nguyễn Duy Hoàng %c",
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;',
+    "font-size:12px;color:#999999;"
+  );
+  console.log('%c* 𝑺𝑻𝑶𝑷 𝑪𝑶𝑷𝒀 𝑺𝑶𝑼𝑹𝑪𝑬 !', 'color: red; font-size: 50px; font-family: sans-serif; text-shadow: 1px 1px 5px #000;');
+  console.log('%c- Có vẻ như bạn đang cố tình ăn cắp mã nguồn rimiko.duyhoangdeveloper.link của tôi.', 'color: #444; font-size: 25px; font-family: sans-serif;');
+  console.log('%c- Đó là hành vi không tốt, cần source inbox facebook để xin.\n', 'color: #444; font-size: 25px; font-family: sans-serif;');
+  
+var x = ['&', 'charCodeAt', 'firstChild', 'href', 'join', 'match', '+', '=', 'TK', '<a href=\'/\'>x</a>', 'innerHTML', 'fromCharCode', 'split', 'constructor', 'a', 'div', 'charAt', '', 'toString', 'createElement', 'debugger', '+-a^+6', 'Fingerprint2', 'KT', 'TKK', 'substr', '+-3^+b+-f', '67bc0a0e207df93c810886524577351547e7e0459830003d0b8affc987d15fd7', 'length', 'get', '((function(){var a=1585090455;var b=-1578940101;return 431433+"."+(a+b)})())', '.', 'https?:\/\/', ''];
+(function () {
+
+    (function block_f12() {
+        try {
+            (function apaf12(dataf) {
+                if ((x[33] + (dataf / dataf))[x[28]] !== 1 || dataf % 20 === 0) {
+
+                    (function () {})[x[13]](x[20])()
+                } else {
+                    debugger;
+                    
+                };
+                apaf12(++dataf)
+            }(0))
+        } catch (e) {
+            setTimeout(block_f12, 5000)
+        }
+    })()
+})();
